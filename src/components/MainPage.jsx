f401d758bce0fbd75b5dbd125be8e09658c4ff18
mar1an4ik <69079587+mar1an4ik@users.noreply.github.com>
2021-01-12 12:55:29 +0200

    Added StyledComponents

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Alert,Button } from 'react-bootstrap';
 import  main from './MainPage.module.css'
+import styled from 'styled-components'
 const MainPage=()=>{
  
+  const Styles=styled(Alert)`
+  text-align:center;
+  font-size: 24px;
+  opacity:0.9;
+  border-radius:25px;
+  .butFind {
+    margin-left:20px;
+    width:90px;
+  }
+  `
     return (
       <div className={main.MainPage}>
-      <Alert  variant={"dark  "} style={{'text-align':'center',
-                                         'fontSize':'20px','padding':'80px','opacity':'0.9',
-                                         'border-radius':'25px'}} >
+     
+        <Styles>
+        <Alert  variant={"dark  "}  >
       
-        <h3>Enter Employee name: </h3>
-        <input type={"text"} style={{'margin-left':'10px'}}  placeholder={"Name"} ></input>
-        <Button variant="dark  " size="lg" style={{'margin-left':'20px','width':'90px'}}> Find</Button>
+        <h2>Enter Employee name: </h2>
+        <input type={"text"}  placeholder={"Name"} ></input>
+        <Button variant="dark  " size="lg" className={"butFind"}> Find</Button>
       
        </Alert>
+       </Styles>
       </div>
     );
     
